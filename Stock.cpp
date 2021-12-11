084724a578e3ebd21bf8e9a449131bbeda8a75f8
ydnalltoowell <danniyun@gmail.com>
2021-12-10 22:43:06 -0500

    CalAR() and CalCR() in Stock class.

--- Stock.cpp
+++ Stock.cpp
@@ -2,15 +2,49 @@
 #include <vector>
 #include <map>
 #include "Stock.h"
+#include <iostream>
+
 using namespace std;
 
 namespace fre {
-	vector<double> Stock::calAR(const Stock& benchmark)const
-	{
-		// calculate abnormal return
+	vector<double> operator-(vector<double> a, const vector<double> b) {
+		for (int i = 0; i < int(a.size()); i++) {
+			a[i] -= b[i];
+		}
+		return a;
 	}
-	vector<double> Stock::calCR()const
-	{
+
+	vector<double> Stock::calAR(const Stock &benchmark, int N) const {
+		// calculate abnormal return
+		vector<string> dates = benchmark.getPriceDate();
+		vector<double> prices = benchmark.getPriceVal();
+
+		int idx = find(dates.begin(), dates.end(), starting_date) - dates.begin();
+		vector<string> new_dates = vector<string>(dates.begin() + idx, dates.begin() + idx + 2 * N + 1);
+		vector<double> new_prices = vector<double>(prices.begin() + idx, prices.begin() + idx + 2 * N + 1);
+
+
+		vector<double> stock_daily_return(2 * N), benchmark_daily_return(2 * N), abnormal_return(2 * N);
+
+		for (int i = 1; i < 2 * N; i++) {
+			cout << stock_daily_return[i] << endl;
+			stock_daily_return[i - 1] = (price_val[i] - price_val[i - 1]) / price_val[i - 1];
+			benchmark_daily_return[i - 1] = (new_prices[i] - new_prices[i - 1]) / new_prices[i - 1];
+			abnormal_return[i - 1] = stock_daily_return[i - 1] - benchmark_daily_return[i - 1];
+		}
+		return abnormal_return;
+
+
+	}
+
+	vector<double> Stock::calCR(const Stock &benchmark, int N) const {
 		// calculate cumulated return
+		vector<double> abnormal_return = calAR(benchmark, N);
+		vector<double> cumulative_return(2 * N);
+		cumulative_return[0] = abnormal_return[0];
+		for (int i = 1; i < 2 * N; i++) {
+			cumulative_return[i] += cumulative_return[i - 1];
+		}
+		return cumulative_return;
 	}
 }
--- Stock.h
+++ Stock.h
@@ -1,76 +1,83 @@
 #pragma once
+
 #include <string>
 #include <vector>
 #include <map>
 
 using namespace std;
 namespace fre {
-	class Stock
-	{
+	class Stock {
 	private:
 		string symbol, name;
 		string date, period_end;
 		vector<double> earning; // {estimate, reported, surprise, surprise%}
 
-		string startingdate, endingdate;
+		string starting_date, ending_date;
 		vector<string> price_date;
 		vector<double> price_val;
-
+		map<string, int> date_price;
 	public:
-		Stock(string symbol_, string name_, string date_, string period_end_, vector<double> earning_): 
-			symbol(symbol_), name(name_), date(date_), period_end(period_end_ ), earning(earning_)
-		{
-			startingdate = "";
-			endingdate = "";
+		Stock(string symbol_, string name_, string date_, string period_end_, vector<double> earning_) :
+				symbol(symbol_), name(name_), date(date_), period_end(period_end_), earning(earning_) {
+			starting_date = "";
+			ending_date = "";
 			price_date = {};
 			price_val = {};
 		}
-		Stock()
-		{
+
+		Stock() {
 			symbol = "";
 			name = "";
 			date = "";
 			period_end = "";
 			earning = {};
-			startingdate = "";
-			endingdate = "";
+			starting_date = "";
+			ending_date = "";
 			price_date = {};
 			price_val = {};
 		}
-		Stock(const Stock& stock)
-		{
+
+		Stock(const Stock &stock) {
 			symbol = stock.symbol;
 			name = stock.name;
 			date = stock.date;
 			period_end = stock.period_end;
 			earning = stock.earning;
-			startingdate = stock.startingdate;
-			endingdate = stock.endingdate;
+			starting_date = stock.starting_date;
+			ending_date = stock.ending_date;
 			price_date = stock.price_date;
 			price_val = stock.price_val;
 		}
-		void updatePrice(vector<string> price_date_, vector<double> price_val_)
-		{
+
+		void updatePrice(vector<string> price_date_, vector<double> price_val_) {
+
 			price_date = price_date_;
 			price_val = price_val_;
-			startingdate = price_date_.front();
-			endingdate = price_date_.back();
+			starting_date = price_date_.front();
+			ending_date = price_date_.back();
 		}
 
-		string getName()const { return name; }
-		string getSymbol()const { return symbol; }
-		string getAnnounceDate()const { return date; }
-		string getPeriodEnd()const { return period_end; }
-		
-		vector<string> getPriceDate()const { return price_date; }
-		vector<double> getPriceVal()const { return price_val; }
-		string getStartingDate()const { return startingdate; }
-		string getEndingDate()const { return endingdate; }
+		string getName() const { return name; }
 
-		vector<double> calAR(const Stock& benchmark) const;
-		vector<double> calCR() const;
+		string getSymbol() const { return symbol; }
 
-		
+		string getAnnounceDate() const { return date; }
+
+		string getPeriodEnd() const { return period_end; }
+
+		vector<string> getPriceDate() const { return price_date; }
+
+		vector<double> getPriceVal() const { return price_val; }
+
+		string getStartingDate() const { return starting_date; }
+
+		string getEndingDate() const { return ending_date; }
+
+		vector<double> calAR(const Stock &benchmark, int N) const;
+
+		vector<double> calCR(const Stock &benchmark, int N) const;
+
+
 	};
 
 }
