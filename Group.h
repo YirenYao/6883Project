ba6a6fe633b241ae047a0fabb83e6a8de7b14b44
Vaanke <81550903+Vaanke@users.noreply.github.com>
2021-12-11 16:01:17 -0500

    Update Group.h
    input ： 
    - stocks map
    - N
    - benchmark 
    - simulate number (for bootstrap)

--- Group.h
+++ Group.h
@@ -9,24 +9,26 @@
 using namespace std;
 
 namespace fre {
-	typedef vector<vector<double>> matrix;
+    typedef vector<vector<map<string, vector<double>>>> matrix; // row : each group, columns : 4 vectors
+    typedef vector<double> Vector;
+    class Group
+    {
+    private:
+        map<string, Stock>* stocks;
+        int N;
+        Vector AAR, AAR_STD, CAAR, CAAR_STD;
 
-	class Group
-	{
-	private:
-		map<string, Stock>* stocks;
-		vector<double> AAR, AAR_STD, CAAR, CAAR_STD;
-	public:
-		Group(map<string, Stock>* stocks_): stocks(stocks_)
-		{
-			srand((unsigned)time(NULL));
-		}
+    public:
+        Group(map<string, Stock>* stocks_, int N_): stocks(stocks_), AAR(2*N_), AAR_STD(2*N_), CAAR(2*N_), CAAR_STD(2*N_), N(N_)
+        {
+            srand((unsigned)time(NULL));
+        }
 
-		vector<double> getAAR()const { return AAR; }
-		vector<double> getAAR_STD()const { return AAR_STD; }
-		vector<double> getCAAR()const { return CAAR; }
-		vector<double> getCAAR_STD()const { return CAAR_STD; }
+        vector<double> getAAR()const { return AAR; }
+        vector<double> getAAR_STD()const { return AAR_STD; }
+        vector<double> getCAAR()const { return CAAR; }
+        vector<double> getCAAR_STD()const { return CAAR_STD; }
 
-		void calByBootstrap();
-	};
+        void calByBootstrap(const Stock &benchmark, int simuNum);
+    };
 }
